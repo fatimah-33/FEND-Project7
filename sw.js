ecb0276e3fb0ea1f8b7ee3174df6e0825d689a67
Fatimah Alqadheep <33449@AYTB-FAB.AYTB.COM>
2019-12-26 15:43:18 +0300

    install server worker

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+// install service worker 
+self.addEventListener('install', evt => {
+    console.log('services worker is installed');
+    
+});
 
+// activate service worker 
+self.addEventListener('activate', evt => {
+    console.log('service worker is activated');
+});
+
+//fetch event
+self.addEventListener('fetch', evt => {
+    console.log('fetch event',evt);
+});
